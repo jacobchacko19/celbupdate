e600b05a58df920b8d7f2d549fb968e96bf30881
Jacob Chacko <jacobchacko@Jacobs-MacBook-Pro.local>
2023-02-03 14:08:23 -0500

    new

---
+++ celebrity.java
@@ -0,0 +1,45 @@
+public class celebrity{
+    
+
+    private String skin; 
+    private String gender; 
+    private String occupation; 
+    private int age; 
+    private int height;
+    private int weight;
+}
+
+    public String getskin()[
+      return skin;
+    ]
+
+    public String getgender()[
+      return gender;
+    ]
+
+    public String getoccupation()[
+      return occupation; 
+    ]
+
+    public int getage()[
+      return age; 
+    ]
+
+    private int getheight()[
+      return height; 
+    ]
+
+    private int getweight()[
+      return weight;
+    ]
+
+    
+
+
+
+
+
+
+
+
+
